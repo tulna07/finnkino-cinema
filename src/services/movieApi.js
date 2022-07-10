02c120fa8225c362f5092cead6ed078e81a3fcb3
scoobytux <tu.lna07@gmail.com>
2022-07-10 19:37:59 +0700

    Add api services

--- src/services/movieApi.js
+++ src/services/movieApi.js
@@ -1,8 +1,30 @@
 import axiosClient from "./config/axiosClient";
 
+const resourceName = "QuanLyPhim/";
+
 const movieApi = {
+  getBannerList: () => {
+    const url = resourceName + "LayDanhSachBanner";
+    return axiosClient.get(url);
+  },
   getMovieList: (params) => {
-    const url = "/QuanLyPhim/LayDanhSachPhim";
+    const url = resourceName + "LayDanhSachPhim";
+    return axiosClient.get(url, { params });
+  },
+  getPaginatedMovieList: (params) => {
+    const url = resourceName + "LayDanhSachPhimPhanTrang";
+    return axiosClient.get(url, { params });
+  },
+  getMovieListByDate: (params) => {
+    const url = resourceName + "LayDanhSachPhimTheoNgay";
+    return axiosClient.get(url, { params });
+  },
+  getMovieDetails: (params) => {
+    const url = resourceName + "LayThongTinPhim";
+    return axiosClient.get(url, { params });
+  },
+  deleteMovie: (params) => {
+    const url = resourceName + "XoaPhim";
     return axiosClient.get(url, { params });
   },
 };
