d795c28f5b308ef8f57b3ef96e248524bd85d85d
scoobytux <tu.lna07@gmail.com>
2022-07-13 22:13:32 +0700

    feat: add authentication with roles required

--- src/guard/index.jsx
+++ src/guard/index.jsx
@@ -1,14 +1,19 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks";
 
-const RequireAuth = ({ children, role }) => {
+const RequireAuth = ({ children, roles }) => {
   const auth = useAuth();
 
   if (!auth.user) {
     return <Navigate to="/auth/login" />;
   }
 
-  return auth.user ? children : <Navigate to="/auth/login" />;
+  const isAllowed = roles?.find((role) => auth.role === role);
+  if (isAllowed) {
+    return children;
+  }
+
+  return <Navigate to="/" />;
 };
 
 export default RequireAuth;
