5af6515bc66b8f37b8028d34c3c822e085caeb3a
scoobytux <tu.lna07@gmail.com>
2022-07-17 23:31:08 +0700

    chore: migrate auth components

--- src/containers/AuthTemplate/AuthCard/index.jsx
+++ src/containers/AuthTemplate/AuthCard/index.jsx
@@ -5,9 +5,9 @@ import FinnkinoLogo from "@/assets/images/header-logo.png";
 
 const AuthCard = ({ children }) => {
   return (
-    <Stack direction="column" alignItems="center" spacing={1} p={6} bgcolor="#fff">
+    <Stack direction="column" alignItems="center" spacing={1} p={5} bgcolor="#fff">
       <Avatar alt="Finnkino logo" src={FinnkinoLogo} sx={{ width: 80, height: 80 }} />
-      <Typography component="h1" variant="h5" fontWeight={700} color="#FFC800">
+      <Typography component="h1" variant="h5" fontSize={30} fontWeight={700} color="#FFC800">
         Log In
       </Typography>
       {children}
