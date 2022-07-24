633cb053a6ac95664e256b813e73f0f38394e6a2
scoobytux <tu.lna07@gmail.com>
2022-07-24 20:04:27 +0700

    style: build ticket booking page without responsive:

--- src/containers/HomeTemplate/TicketBookingPage/SeatSelector/index.jsx
+++ src/containers/HomeTemplate/TicketBookingPage/SeatSelector/index.jsx
@@ -1,5 +1,5 @@
 // Material UI
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, Stack } from "@mui/material";
 
 // Scss
 import "./style.scss";
@@ -8,7 +8,7 @@ const SeatSelector = () => {
   const arr = [];
   for (let i = 0; i < 160; ++i) {
     arr.push(
-      <Grid item xs={1}>
+      <Grid item xs={1} key={i}>
         <Box className="seat-selector__seat">{i + 1}</Box>
       </Grid>,
     );
@@ -28,6 +28,36 @@ const SeatSelector = () => {
           {arr}
         </Grid>
       </Box>
+      <Grid
+        className="seat-selector__seat-notes-container"
+        container
+        sx={{ color: "black", px: 5, mt: 2 }}
+      >
+        <Grid item xs={3}>
+          <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
+            <Box className="seat-selector__seat-notes seat-selector__seat-notes--selected"></Box>
+            <Typography>Ghế đang chọn</Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={3}>
+          <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
+            <Box className="seat-selector__seat-notes seat-selector__seat-notes--sold"></Box>
+            <Typography>Ghế đã bán</Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={3}>
+          <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
+            <Box className="seat-selector__seat-notes seat-selector__seat-notes--selectable"></Box>
+            <Typography>Có thể chọn</Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={3}>
+          <Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
+            <Box className="seat-selector__seat-notes seat-selector__seat-notes--unavailable"></Box>
+            <Typography>Không thể chọn</Typography>
+          </Stack>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
